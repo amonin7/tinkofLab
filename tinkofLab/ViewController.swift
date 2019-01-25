126c943be2fb5166f0a53b0c8e80623bb99f1450
Andrey Minin <ewkjf@mail.ru>
2019-01-26 00:37:37 +0300

    1 commit

--- tinkofLab/ViewController.swift
+++ tinkofLab/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource {
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    // MARK: - data for picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+    
 
 }
 
