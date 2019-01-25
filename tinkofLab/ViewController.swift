d23948cc6da9a33989cc3431b385d4d66348785b
Andrey Minin <ewkjf@mail.ru>
2019-01-26 00:18:40 +0300

    Initial Commit

---
+++ tinkofLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tinkofLab
+//
+//  Created by Andrey Minin on 26/01/2019.
+//  Copyright © 2019 Andrey Minin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
